8430e83b8cc6c42d16da62d9e5e3592548a4b258
kuviman <kuviman@gmail.com>
2024-10-05 15:13:57 +0400

    flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    geng.url = "github:geng-engine/cargo-geng";
+    geng.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = { geng, nixpkgs, systems, self }:
+    let
+      pkgsFor = system: import nixpkgs { inherit system; };
+      forEachSystem = f: nixpkgs.lib.genAttrs (import systems) (system:
+        let pkgs = pkgsFor system;
+        in f system pkgs);
+    in
+    {
+      devShells = forEachSystem (system: pkgs:
+        {
+          default = geng.lib.mkShell {
+            inherit system;
+            target.linux.enable = true;
+            target.web.enable = true;
+          };
+        });
+      formatter = forEachSystem (system: pkgs: pkgs.nixpkgs-fmt);
+    };
+}
