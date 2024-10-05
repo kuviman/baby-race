15591ba89a6dbdc4bf76b48f812e4ca01551d155
kuviman <kuviman@gmail.com>
2024-10-05 15:50:29 +0400

    Baby has been drawn

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,76 @@ struct CliArgs {
     geng: geng::CliArgs,
 }
 
+#[derive(Deserialize)]
+struct BabyConfig {
+    radius: f32,
+    head_offset: vec2<f32>,
+    arm_offset: vec2<f32>,
+    leg_offset: vec2<f32>,
+}
+
 #[derive(geng::asset::Load, Deserialize)]
 #[load(serde = "toml")]
 struct Config {
     background_color: Rgba<f32>,
+    fov: f32,
+    baby: BabyConfig,
+}
+
+#[derive(Deref)]
+struct Texture(ugli::Texture);
+
+impl std::borrow::Borrow<ugli::Texture> for &Texture {
+    fn borrow(&self) -> &ugli::Texture {
+        &self.0
+    }
+}
+
+impl geng::asset::Load for Texture {
+    type Options = ();
+    fn load(
+        manager: &geng::asset::Manager,
+        path: &std::path::Path,
+        _options: &Self::Options,
+    ) -> geng::asset::Future<Self> {
+        let texture = manager.load(path);
+        async move {
+            let mut texture: ugli::Texture = texture.await?;
+            texture.set_filter(ugli::Filter::Nearest);
+            Ok::<_, anyhow::Error>(Self(texture))
+        }
+        .boxed_local()
+    }
+
+    const DEFAULT_EXT: Option<&'static str> = Some("png");
+}
+
+#[derive(geng::asset::Load)]
+struct BabyAssets {
+    body: Texture,
+    head: Texture,
+    arm: Texture,
+    leg: Texture,
 }
 
 #[derive(geng::asset::Load)]
 struct Assets {
     config: Config,
+    baby: BabyAssets,
+}
+
+struct Baby {
+    pos: vec2<f32>,
+    rotation: Angle<f32>,
+    radius: f32,
 }
 
 struct Game {
     geng: Geng,
     assets: Rc<Assets>,
+    baby: Baby,
+    camera: Camera2d,
+    time: f32,
 }
 
 impl Game {
@@ -28,7 +84,65 @@ impl Game {
         Self {
             geng: geng.clone(),
             assets: assets.clone(),
+            baby: Baby {
+                pos: vec2::ZERO,
+                rotation: Angle::ZERO,
+                radius: assets.config.baby.radius,
+            },
+            camera: Camera2d {
+                center: vec2::ZERO,
+                rotation: Angle::ZERO,
+                fov: Camera2dFov::MinSide(assets.config.fov),
+            },
+            time: 0.0,
         }
+    }
+
+    fn draw_baby(&self, framebuffer: &mut ugli::Framebuffer, baby: &Baby) {
+        let transform = mat3::translate(baby.pos)
+            * mat3::rotate(baby.rotation)
+            * mat3::scale_uniform(baby.radius);
+        self.geng.draw2d().draw2d(
+            framebuffer,
+            &self.camera,
+            &draw2d::TexturedQuad::unit(&self.assets.baby.arm)
+                .transform(transform * mat3::translate(self.assets.config.baby.arm_offset)),
+        );
+        self.geng.draw2d().draw2d(
+            framebuffer,
+            &self.camera,
+            &draw2d::TexturedQuad::unit(&self.assets.baby.arm).transform(
+                transform
+                    * mat3::scale(vec2(-1.0, 1.0))
+                    * mat3::translate(self.assets.config.baby.arm_offset),
+            ),
+        );
+        self.geng.draw2d().draw2d(
+            framebuffer,
+            &self.camera,
+            &draw2d::TexturedQuad::unit(&self.assets.baby.leg)
+                .transform(transform * mat3::translate(self.assets.config.baby.leg_offset)),
+        );
+        self.geng.draw2d().draw2d(
+            framebuffer,
+            &self.camera,
+            &draw2d::TexturedQuad::unit(&self.assets.baby.leg).transform(
+                transform
+                    * mat3::scale(vec2(-1.0, 1.0))
+                    * mat3::translate(self.assets.config.baby.leg_offset),
+            ),
+        );
+        self.geng.draw2d().draw2d(
+            framebuffer,
+            &self.camera,
+            &draw2d::TexturedQuad::unit(&self.assets.baby.body).transform(transform),
+        );
+        self.geng.draw2d().draw2d(
+            framebuffer,
+            &self.camera,
+            &draw2d::TexturedQuad::unit(&self.assets.baby.head)
+                .transform(transform * mat3::translate(self.assets.config.baby.head_offset)),
+        );
     }
 }
 
@@ -40,6 +154,7 @@ impl geng::State for Game {
             None,
             None,
         );
+        self.draw_baby(framebuffer, &self.baby);
     }
 }
 
